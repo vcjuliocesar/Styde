2d5ac6cf1ee61ed70ff593401df35e47b3cd7eda
Julio Cesar <vcjuliocesar@outlook.com>
2018-03-29 18:59:39 -0600

    lession21

---
+++ lession21/public/index.php
@@ -0,0 +1,13 @@
+<?php
+require '../vendor/autoload.php';
+use Styde\User;
+use Styde\LunchBox;
+
+$gordon = new User(['name' => 'Gordon']);
+// Daughter
+$joanie = new User(['name' => 'Joanie']);
+$lunchBox = new LunchBox(['Sandwich', 'Papas', 'Jugo de naranja', 'Manzana']);
+// House
+$joanie->setLunch($lunchBox);
+// School
+$joanie->eatMeal();
